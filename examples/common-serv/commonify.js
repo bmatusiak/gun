b5c1ea224380d4de362ef24c91aa84ad0a69f409
Bradley Matusiak <bmatusiak@gmail.com>
2020-01-04 11:30:15 -0500

    added common wrap server example for dev

---
+++ examples/common-serv/commonify.js
@@ -0,0 +1,61 @@
+var path = require("path");
+
+const fs = require('fs')
+
+function wrapCommon(contents){
+    var output = "";
+    //-------------------------
+    output += "define(function(require, exports, module) {\n"; 
+    
+    output += contents;
+    
+    output += "\n});";
+    
+    return output;
+}
+
+function lookupReadPath(path,callback){
+
+    fs.exists(path, function(exists) {
+        if (!exists)
+            callback(true);
+        else {
+            fs.readFile(path, 'utf8', function(err, contents) {
+                if(!err)
+                    callback(null,contents);
+                else 
+                    callback(true);
+            });
+        }
+
+    });
+}
+
+module.exports = function(commonDir) {
+
+    return function(req, res, next) {
+        var path = commonDir + req.url;
+        
+        function done(data){
+            if(data){
+                res.send(wrapCommon(data));
+            }else next();
+        }
+        
+        lookupReadPath(path, function(err, content){//lookup base path
+            if(err){
+                lookupReadPath(path+".js", function(err, content){//look here just for fun
+                    if(err){
+                        done();//should i alto try removing .js too just incase of .js.js?  (because of requirejs)?
+                    }else{
+                        done(content);
+                    }
+                });
+            }else{
+                done(content);
+            }
+        });
+
+    };
+
+};
---
+++ examples/common-serv/server.js
@@ -0,0 +1,42 @@
+var path = require("path");
+var GUNDIR = path.resolve(__dirname + "/../../");
+
+
+var selfsigned = process.env.SSL ? require('selfsigned') : false; //require('selfsigned');
+var port = process.env.PORT || 8765;
+var https = require('https');
+var http = require('http');
+var express = require('express');
+var Gun = require('gun');
+require('gun/axe');
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use("/gun/examples",express.static(GUNDIR + "/examples"));
+
+var commonify = require("./commonify.js");
+app.use('/gunjs/', commonify(GUNDIR));
+
+//app.use(Gun.serve);//<-- server normmaly
+
+var server;
+
+if (selfsigned) {
+    var attrs = null; //[{ name: 'commonName', value: '127.0.0.1' }];
+    var pems = selfsigned.generate(attrs, { days: 365 });
+    // we will pass our 'app' to 'https' server
+
+    server = https.createServer({
+            key: pems.private,
+            cert: pems.cert
+        }, app)
+        .listen(port);
+}
+else {
+    server = http.createServer(app)
+        .listen(port);
+}
+
+var gun = Gun({ file: 'data', web: server });
+
+console.log('Server started on port ' + port + ' with /gun');
